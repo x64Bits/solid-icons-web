d52af08fc296f92d28a0f4e73a5d8dd0a5df19d7
Ignacio Zsabo <ignacioluisr2006@gmail.com>
2022-09-07 22:53:39 -0400

    feat: add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import solid from "solid-start";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ adapter: vercel() })],
   resolve: {
     alias: {
       "@public": "public",
