4dfb0202dfcf2273a78728ac9ee2aad60bcf5a9e
Ignacio Zsabo <ignacioluisr2006@gmail.com>
2022-09-16 17:29:45 -0400

    fix: remember vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import solid from "solid-start/vite";
-// import vercel from "solid-start-vercel";
-import node from "solid-start-node";
+import vercel from "solid-start-vercel";
+// import node from "solid-start-node";
 import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  // plugins: [solid({ adapter: vercel({ edge: true }) })],
+  plugins: [solid({ adapter: vercel({ edge: true }) })],
   // Local, uncomment next line
-  plugins: [solid({ adapter: node() })],
+  // plugins: [solid({ adapter: node() })],
   resolve: {
     alias: {
       "@public": "public",
