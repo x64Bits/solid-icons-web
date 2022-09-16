c94814cd0e00d22668222f492bcad462c491bb4d
Ignacio Zsabo <ignacioluisr2006@gmail.com>
2022-09-16 17:07:46 -0400

    fix: ship vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import solid from "solid-start/vite";
-import netlify from "solid-start-netlify";
+// import vercel from "solid-start-vercel";
+import node from "solid-start-node";
 import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [solid({ adapter: netlify() })],
-  // plugins: [solid({ adapter: node() })],
+  // plugins: [solid({ adapter: vercel({ edge: true }) })],
+  // Local, uncomment next line
+  plugins: [solid({ adapter: node() })],
   resolve: {
     alias: {
       "@public": "public",
